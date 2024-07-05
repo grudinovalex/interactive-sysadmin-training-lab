448f2bf703f350f219d51b9d297ecf253413a471
axg536 <axg536@student.bham.ac.uk>
2024-07-05 12:08:12 +0100

    added dropdown menu into navbar

--- mounts/flask/app/views.py
+++ mounts/flask/app/views.py
@@ -19,37 +19,10 @@ def navbar():
 
 @app.route('/showcow')
 def showcow():
-    hostname = "istl"
-    username = "trainee"
-    password = "istl2024"
-
-    # Create an SSH client instance
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    # Connect to the server
-    client.connect(hostname, port=22, username=username, password=password)
-
-    # Execute commands on the server
-    stdin, stdout, stderr = client.exec_command("ls -ld /proc")
-    stdin.close()
-    output = stdout.read()
-    output = output.decode("utf-8")
-    # output = output[:(len(output) - 1)]
-
-    # Print the output of the command
-    # print(output)
-
-    # Close the SSH connection
-    client.close()
-
     stdout, stderr = cmd("cat cow.txt")
-
-    return render_template('showcow.html', cow=output, cow2=stdout)
+    return render_template('showcow.html', cow2=stdout, title="Show the cow in cat.txt")
 
 @app.route('/fabrictest')
 def fabrictest():
-#    connection = Connection(host = "trainee@istl", connect_kwargs = {"password" : "istl2024"})
     output = connection.run("ls -ld /proc", hide = True).stdout.strip()
-
-    return render_template('fabrictest.html', message=output)
+    return render_template('fabrictest.html', message=output, title="Test the fabric module")
